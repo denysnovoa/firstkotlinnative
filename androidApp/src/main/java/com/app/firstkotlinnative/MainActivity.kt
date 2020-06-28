e0d6603d8952bb81f24f8a7e0919a1163c008498
Denys <dnovoa20@gmail.com>
2020-06-28 14:27:17 +0200

    implemented android version

--- androidApp/src/main/java/com/app/firstkotlinnative/MainActivity.kt
+++ androidApp/src/main/java/com/app/firstkotlinnative/MainActivity.kt
@@ -2,10 +2,17 @@ package com.app.firstkotlinnative
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
   override fun onCreate(savedInstanceState: Bundle?) {
+
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
+
+    val greetingTex = this.findViewById<TextView>(R.id.greetingTitle)
+
+    greetingTex.text = Greeting().greeting()
   }
 }
