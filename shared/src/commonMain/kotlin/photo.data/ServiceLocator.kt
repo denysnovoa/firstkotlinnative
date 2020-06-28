46306996aa88c8fdde9d5e8927e9824752cbd3e3
Denys <dnovoa20@gmail.com>
2020-06-28 18:08:55 +0200

    fixed import

--- shared/src/commonMain/kotlin/photo.data/ServiceLocator.kt
+++ shared/src/commonMain/kotlin/photo.data/ServiceLocator.kt
@@ -1,21 +1,26 @@
 package com.app.firstkotlinnative.photo.data
 
+import com.app.firstkotlinnative.photo.data.api.PhotoApi
+import com.app.firstkotlinnative.photo.data.api.PhotoApiService
+import com.app.firstkotlinnative.photo.data.model.PhotoResponse
+import io.ktor.client.HttpClient
+import io.ktor.client.features.json.JsonFeature
+
 object ServiceLocator {
 
-//  private fun provideHttpClient(): HttpClient {
-//    return HttpClient {
-//      install(JsonFeature) {
+  private fun provideHttpClient(): HttpClient {
+    return HttpClient {
+      install(JsonFeature) {
 //        serializer = KotlinxSerializer(JSON.nonstrict).apply {
 //          setMapper(PhotoResponse::class, PhotoResponse.serializer())
 //        }
-//      }
-//      install(ExpectSuccess)
-//    }
-//  }
-//
-//  private fun provideApiHost() = "https://api.unsplash.com"
-//  private fun provideClientId() = "7b62093f7803511c09584f0d978630a4ab3d5f1b3f5c70f97c30f6dbef25fdbf"
-//
-//  fun providePhotoApiService(): PhotoApi =
-//    PhotoApiService(provideHttpClient(), provideApiHost(), provideClientId())
+      }
+    }
+  }
+
+  private fun provideApiHost() = "https://api.unsplash.com"
+  private fun provideClientId() = "7b62093f7803511c09584f0d978630a4ab3d5f1b3f5c70f97c30f6dbef25fdbf"
+
+  fun providePhotoApiService(): PhotoApi =
+    PhotoApiService(provideHttpClient(), provideApiHost(), provideClientId())
 }
