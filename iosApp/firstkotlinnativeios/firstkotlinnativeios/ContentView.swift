107db5fb0c9352774feccdb0c698f61433ca4a63
Denys <dnovoa20@gmail.com>
2020-06-28 14:33:30 +0200

    added iosApp

---
+++ iosApp/firstkotlinnativeios/firstkotlinnativeios/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  firstkotlinnativeios
+//
+//  Created by Denys Ricardo Novoa López on 28/06/2020.
+//  Copyright © 2020 Denys. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
